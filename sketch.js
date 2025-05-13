78ee33956d901fde7391a38b0f773d1fc282e653
Nathan E A Bevilaqua <nathanarrotheia@gmail.com>
2025-05-13 01:16:26 -0300

    v003

--- sketch.js
+++ sketch.js
@@ -1,45 +1,50 @@
 var x = [0, 0, 0, 0]
 var y = []
 var tipo = ["🃏", "👾", "🐱", "😤"]
-var botao = ["a", "f", "j", "ç"]
+var botao = ["a", "f", "j", "ç"] // teclas usadas no jogo
 var quant = tipo.length
 let ganhador = [];
 let termi = false;
+let botaoHTML; // variável global para o botão real
 
 function setup() {
-  createCanvas(windowWidth, windowHeight) 
-  y = [height*1/5, height*2/5, height*3/5, height*4/5]
+  let canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent("jogo");
+  y = [height*1/5, height*2/5, height*3/5, height*4/5];
+
+  botaoHTML = select("#recomeca"); // usa o nome diferente
+  botaoHTML.position(canvas.elt.offsetLeft + width/2 - 115, canvas.elt.offsetTop + height/2);
+  botaoHTML.hide();
 }
 
 function draw() {
-  comeco()
-  perso()
-  chegada()
-  corra()
-  repete()
+  comeco();
+  perso();
+  chegada();
+  corra();
+  repete(); // mostra o botão se necessário
 }
 
 function comeco() {
   if (focused == true) {
-    background('#d00')
+    background('#d00');
   } else {
-    background('#900')
+    background('#900');
   }
 }
 
 function chegada() {
   fill("white");
-  rect(width-height/10, 0, 20, 800);
+  rect(width - height/10, 0, 20, height);
   
   fill("black");
-  for (let quadrado = 0; quadrado < 800;
-      quadrado +=20){
-    rect(width-height/10, quadrado, 20, 10);
+  for (let quadrado = 0; quadrado < height; quadrado += 20){
+    rect(width - height/10, quadrado, 20, 10);
   }
 }
 
 function perso() {
-  textSize(height/10)
+  textSize(height/10);
   for (let posição = 0; posição < quant; posição++){
     text(tipo[posição], x[posição], y[posição]);
   }
@@ -47,10 +52,10 @@ function perso() {
 
 function corra() {
   if (!termi) {
-    ganhador = []
+    ganhador = [];
 
     for (let posição = 0; posição < quant; posição++) {
-      if(x[posição] > width-height/10) {
+      if (x[posição] > width - height/10) {
         ganhador.push(tipo[posição]);
       }
     }
@@ -65,8 +70,8 @@ function corra() {
     fill("black");
     textSize(36);
     textAlign(CENTER, CENTER);
-    let texto = ganhador.join(" e ") + " venceu!"
-    text(texto, width/2, height/2-36);
+    let texto = ganhador.join(" e ") + " venceu!";
+    text(texto, width/2, height/2 - 36);
   }
 }
 
@@ -82,30 +87,19 @@ function keyReleased(){
 
 function repete() {
   if (termi){
-    fill("white");
-    stroke("black");
-    rect(width/2-65, height/2, 150, 50, 15);
-    
-    fill("red");
-    textSize(28);
-    textAlign(CENTER, CENTER);
-    text("Reiniciar?", width/2+10, height/2+27);
+    botaoHTML.show();
   }
 }
 
-function mousePressed(){
-  if (termi){
-    if(mouseX > width/2-65 && mouseX > width/2 + 85 && mouseY > height/2 && mouseY < height/2 + 50){
-      reiniciarjogo();
-    }
-  }
-}
+// ATENÇÃO: sem parênteses aqui!
+botaoHTML.addEventListener("click", recomeca);
 
-function reiniciarjogo() {
+function recomeca() {
   for (let posição = 0; posição < quant; posição++){
     x[posição] = 0;
-    ganhador = [];
-    termi = false;
-    loop();
   }
+  ganhador = [];
+  termi = false;
+  botaoHTML.hide();
+  loop();
 }
